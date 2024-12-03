d59d122fffbd80507f5cfb13284cbe2e88c21089
khaled <khaledmohamed942@gmail.com>
2024-12-03 11:57:16 +0200

    adjust stracture and build some components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'logIn', pathMatch: 'full' },
+  {
+    path: 'Home',
+    loadComponent: () =>
+      import('./layout/home-page/home-page.component').then(
+        (c) => c.HomePageComponent
+      ),
+  },
+  {
+    path: 'logIn',
+    loadComponent: () =>
+      import('./auth/log-in/log-in.component').then(
+        (c) => c.LogInComponent
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./shared/components/not-found/not-found.component').then(
+        (c) => c.NotFoundComponent
+      ),
+  },
+];
---
+++ src/app/shared/components/fullcalendar/fullcalendar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fullcalendar',
+  imports: [],
+  templateUrl: './fullcalendar.component.html',
+  styleUrl: './fullcalendar.component.scss'
+})
+export class FullcalendarComponent {
+
+}
