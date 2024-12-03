3e2412735397a0bdc3255f86fe7b1ddb699c87b5
khaled <khaledmohamed942@gmail.com>
2024-12-03 14:44:55 +0200

    firebase

--- src/app/auth/log-in/log-in.component.ts
+++ src/app/auth/log-in/log-in.component.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  Auth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-log-in',
-  imports: [],
-  standalone:true,
+  imports: [FormsModule],
+  standalone: true,
   templateUrl: './log-in.component.html',
-  styleUrl: './log-in.component.scss'
+  styleUrl: './log-in.component.scss',
 })
 export class LogInComponent {
+  phoneNumber: string = '';
+  otp: string = '';
+  otpSent = false;
+  errorMessage: string | null = null;
+  private recaptchaVerifier: RecaptchaVerifier | undefined;
+  private confirmationResult: any;
 
+  constructor(private auth: Auth) {}
+
+  sendOTP() {
+    this.errorMessage = null;
+
+    this.recaptchaVerifier = new RecaptchaVerifier(
+      this.auth,
+      'recaptcha-container',
+      { size: 'invisible' }
+    );
+
+    signInWithPhoneNumber(this.auth, this.phoneNumber, this.recaptchaVerifier)
+      .then((confirmationResult) => {
+        this.otpSent = true;
+        this.confirmationResult = confirmationResult;
+      })
+      .catch((error) => {
+        this.errorMessage = error.message;
+      });
+  }
+
+  verifyOTP() {
+    if (!this.confirmationResult) {
+      this.errorMessage = 'No OTP was sent!';
+      return;
+    }
+
+    this.confirmationResult
+      .confirm(this.otp)
+      .then((result: any) => {
+        console.log('User signed in successfully:', result.user);
+      })
+      .catch((error: any) => {
+        this.errorMessage = error.message;
+      });
+  }
 }
