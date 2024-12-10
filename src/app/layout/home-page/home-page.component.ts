51732cd3c94bee558a8a9cc793676b991473922d
Nabil El-Nour <nbailbolbol@gmail.com>
2024-12-10 13:15:57 +0200

    Nav-bar

--- src/app/layout/home-page/home-page.component.ts
+++ src/app/layout/home-page/home-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { FullcalendarComponent } from '../../shared/components/fullcalendar/fullcalendar.component';
-
+import { NavBarComponent } from '../../layout/nav-bar/nav-bar.component';
 @Component({
   selector: 'app-home-page',
-  imports: [FullcalendarComponent],
+  imports: [FullcalendarComponent, NavBarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   standalone: true,
--- src/app/layout/nav-bar/nav-bar.component.ts
+++ src/app/layout/nav-bar/nav-bar.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+import { Router } from '@angular/router';
 @Component({
-  selector: 'app-nav-bar',
-  imports: [],
+  selector: 'appNavbar',
+  imports: [MenuModule, ButtonModule, AvatarModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
-  standalone:true,
+  standalone: true,
 })
 export class NavBarComponent {
+  constructor(private router: Router) {}
+  menuItems = [
+    {
+      label: 'Settings',
+      icon: 'pi pi-cog',
+      command: () => this.navigateToSettings(),
+    },
+    { label: 'Logout', icon: 'pi pi-sign-out', command: () => this.logout() },
+  ];
 
+  navigateToSettings() {
+    console.log('Navigating to settings...');
+    // Add navigation logic here
+  }
+
+  logout() {
+    console.log('Logging out...');
+    this.router.navigate(['/login']);
+    // Add logout logic here
+  }
 }
